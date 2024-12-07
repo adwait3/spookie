f3d0e99ace7a28e06188853f0d825bf3c1c68396
IC3lemon <madhavlemon@gmail.com>
2024-12-07 14:44:31 +0530

    model and sized stuff up

--- core/src/main/java/io/github/spookie/Main.java
+++ core/src/main/java/io/github/spookie/Main.java
@@ -16,18 +16,21 @@ import com.badlogic.gdx.utils.ScreenUtils;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     public PerspectiveCamera cam;
-    public AssetManager assets;
     public Environment environment;
     public ModelBatch modelBatch;
+    public Model alphabetsModel;
+    public ModelInstance alphabetsInstance;
+    private AssetManager assetManager;
     public Model floorModel, wallModel, leftWallModel, rightWallModel, backWallModel, frontWallModel;
     public ModelInstance floorInstance, wallInstance, leftWallInstance, rightWallInstance, backWallInstance, frontWallInstance;
 
-    private float movementSpeed = 15f;
+    private float movementSpeed = 500f;
     private Vector3 movement = new Vector3();
     private ShapeRenderer shapeRenderer;
 
     @Override
     public void create () {
+        assetManager = new AssetManager();
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
@@ -35,25 +38,31 @@ public class Main extends ApplicationAdapter {
         cam.position.set(0f, 5f, 0f);
 
         cam.near = 1f;
-        cam.far = 300f;
+        cam.far = 5000f;
         cam.update();
+
+        assetManager.load("alphabets.g3db", Model.class);
+        assetManager.finishLoading();
+        alphabetsModel = assetManager.get("alphabets.g3db", Model.class);
+        alphabetsInstance = new ModelInstance(alphabetsModel);
+        alphabetsInstance.transform.setToTranslation(0f, 2f, 0f);
 
         modelBatch = new ModelBatch();
         shapeRenderer = new ShapeRenderer();
 
         ModelBuilder modelBuilder = new ModelBuilder();
         // modelBuilder.begin();
-        floorModel = modelBuilder.createBox(100f, 1f, 100f,
+        floorModel = modelBuilder.createBox(2000f, 1f, 2000f,
             new Material(ColorAttribute.createDiffuse(Color.GRAY)),
             VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         floorInstance = new ModelInstance(floorModel);
 
-        wallModel = modelBuilder.createBox(1f, 20f, 101f,
+        wallModel = modelBuilder.createBox(1f, 500f, 2001f,
             new Material(ColorAttribute.createDiffuse(Color.valueOf("#e4e6a8"))),
             VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         frontWallModel = wallModel;
         backWallModel = wallModel;
-        leftWallModel = modelBuilder.createBox(101f, 20f, 1f,
+        leftWallModel = modelBuilder.createBox(2001f, 500f, 1f,
             new Material(ColorAttribute.createDiffuse(Color.valueOf("#e4e6a8"))),
             VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
         rightWallModel = leftWallModel;
@@ -62,10 +71,10 @@ public class Main extends ApplicationAdapter {
         leftWallInstance = new ModelInstance(leftWallModel);
         rightWallInstance = new ModelInstance(rightWallModel);
 
-        frontWallInstance.transform.setToTranslation(50f, 10f, 0);
-        backWallInstance.transform.setToTranslation(-50f, 10f, 0);
-        leftWallInstance.transform.setToTranslation(0, 10f, -50f);
-        rightWallInstance.transform.setToTranslation(0f, 10f, 50f);
+        frontWallInstance.transform.setToTranslation(1000f, 250f, 0);
+        backWallInstance.transform.setToTranslation(-1000f, 250f, 0);
+        leftWallInstance.transform.setToTranslation(0, 250f, -1000f);
+        rightWallInstance.transform.setToTranslation(0f, 250f, 1000f);
 
     }
 
@@ -82,10 +91,10 @@ public class Main extends ApplicationAdapter {
         modelBatch.render(backWallInstance, environment);
         modelBatch.render(leftWallInstance, environment);
         modelBatch.render(rightWallInstance, environment);
+        modelBatch.render(alphabetsInstance, environment);
         modelBatch.end();
         drawButtons();
     }
-
     private void drawButtons(){
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.GRAY);
@@ -140,6 +149,12 @@ public class Main extends ApplicationAdapter {
         rightWallModel.dispose();
         backWallModel.dispose();
         frontWallModel.dispose();
+        if (alphabetsModel != null) {
+            alphabetsModel.dispose();
+        }
+        if (assetManager != null) {
+            assetManager.dispose();
+        }
     }
     @Override
     public void resume () {
